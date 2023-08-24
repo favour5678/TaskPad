c99350d533711ec4699ce819f65bae6a9fe4cfac
Oshoaro Favour <oshoarofavour@gmail.com>
2023-08-24 16:52:54 +0100

    just designed the left sectionof the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { IoEllipseSharp } from 'react-icons/io5'
+import { BsFilterLeft } from 'react-icons/bs'
+import userIcon from '../images/userIcon.png'
 
 const Dashboard = () => {
+    
+
   return (
-    <div>Dashboard</div>
+    <section className='bg-red-100 container max-w-full h-screen'>
+       <div className='bg-green-50 w-[22%] h-full shadow'>
+       <div className='flex items-center justify-center space-x-4'>
+            <img src={userIcon} alt="user-icon" className='h-12 bg-white rounded-full'/>
+            <p className='font-bold text-lg'>Hello User &#128075;</p>
+        </div>
+        <button className='flex justify-center items-center mx-auto space-x-4 bg-green-400 w-[242px] h-[48px] rounded-full'>
+            <AiOutlinePlus className='text-xl'/>
+            <p className='text-lg font-bold'>Create task</p>
+        </button>
+        <p className='font-bold text-xl bg-green-200 h-[48px] flex items-center pl-4'>Calendar</p>
+        <div>
+            <p className='font-bold text-xl'>Sort</p>
+            <ol>
+                <li className='font-semibold'><IoEllipseSharp className='text-green-400 text-sm'/>Day</li>
+                <li className='font-semibold'><IoEllipseSharp className='text-yellow-400 text-sm'/>Month</li>
+                <li className='font-semibold'><IoEllipseSharp className='text-blue-400 text-sm'/>Year</li>
+            </ol>
+        </div>
+        <BsFilterLeft className='text-3xl'/>
+       </div>
+    </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import taskImage from "../assets/taskImage.png";
+import taskImage from "../images/taskImage.png";
 import { Link } from "react-router-dom";
 
 const Home = () => {
