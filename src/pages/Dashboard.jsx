6a4994fdc58fe87def452c61872c6115c50b8760
Oshoaro Favour <oshoarofavour@gmail.com>
2023-08-24 18:13:32 +0100

    design for the left section on dashboard completed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,25 +9,27 @@ const Dashboard = () => {
 
   return (
     <section className='bg-red-100 container max-w-full h-screen'>
-       <div className='bg-green-50 w-[22%] h-full shadow'>
-       <div className='flex items-center justify-center space-x-4'>
+       <div className='bg-green-50 w-[20%] h-full shadow space-y-10 pt-6'>
+       <div className='flex items-center justify-center space-x-6'>
             <img src={userIcon} alt="user-icon" className='h-12 bg-white rounded-full'/>
             <p className='font-bold text-lg'>Hello User &#128075;</p>
         </div>
-        <button className='flex justify-center items-center mx-auto space-x-4 bg-green-400 w-[242px] h-[48px] rounded-full'>
+        <button className='flex justify-center items-center mx-auto space-x-4 bg-green-400 w-[230px] h-[48px] rounded-full'>
             <AiOutlinePlus className='text-xl'/>
             <p className='text-lg font-bold'>Create task</p>
         </button>
-        <p className='font-bold text-xl bg-green-200 h-[48px] flex items-center pl-4'>Calendar</p>
-        <div>
+        <p className='font-bold text-xl bg-green-200 h-[48px] flex items-center pl-6'>Calendar</p>
+        <div className='space-y-3 pl-6'>
             <p className='font-bold text-xl'>Sort</p>
             <ol>
-                <li className='font-semibold'><IoEllipseSharp className='text-green-400 text-sm'/>Day</li>
-                <li className='font-semibold'><IoEllipseSharp className='text-yellow-400 text-sm'/>Month</li>
-                <li className='font-semibold'><IoEllipseSharp className='text-blue-400 text-sm'/>Year</li>
+                <li className='font-semibold flex items-center'><IoEllipseSharp className='text-green-400 text-sm mr-2'/>Day</li>
+                <li className='font-semibold flex items-center'><IoEllipseSharp className='text-yellow-400 text-sm mr-2'/>Month</li>
+                <li className='font-semibold flex items-center'><IoEllipseSharp className='text-blue-400 text-sm mr-2'/>Year</li>
             </ol>
         </div>
-        <BsFilterLeft className='text-3xl'/>
+        <p className='pt-28'>
+            <BsFilterLeft className='text-4xl ml-6 text-green-400'/>
+        </p>
        </div>
     </section>
   )
