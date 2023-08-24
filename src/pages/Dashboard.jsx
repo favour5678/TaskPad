8e43c001fa881aac0329a5b0b08dc326fa3dccfa
Oshoaro Favour <oshoarofavour@gmail.com>
2023-08-24 19:28:08 +0100

    added an external file to the tailwind config and worked on the right section on the dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,38 +1,57 @@
-import React from 'react'
-import { AiOutlinePlus } from 'react-icons/ai'
-import { IoEllipseSharp } from 'react-icons/io5'
-import { BsFilterLeft } from 'react-icons/bs'
-import userIcon from '../images/userIcon.png'
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { IoEllipseSharp } from "react-icons/io5";
+import { BsFilterLeft } from "react-icons/bs";
+import userIcon from "../images/userIcon.png";
 
 const Dashboard = () => {
-    
-
   return (
-    <section className='bg-red-100 container max-w-full h-screen'>
-       <div className='bg-green-50 w-[20%] h-full shadow space-y-10 pt-6'>
-       <div className='flex items-center justify-center space-x-6'>
-            <img src={userIcon} alt="user-icon" className='h-12 bg-white rounded-full'/>
-            <p className='font-bold text-lg'>Hello User &#128075;</p>
+    <section className="container max-w-full h-screen flex">
+      <div className="bg-green-50 w-[20%] h-full shadow space-y-10 pt-6">
+        <div className="flex items-center justify-center space-x-6">
+          <img
+            src={userIcon}
+            alt="user-icon"
+            className="h-12 bg-white rounded-full"
+          />
+          <p className="font-bold text-lg">Hello User &#128075;</p>
         </div>
-        <button className='flex justify-center items-center mx-auto space-x-4 bg-green-400 w-[230px] h-[48px] rounded-full'>
-            <AiOutlinePlus className='text-xl'/>
-            <p className='text-lg font-bold'>Create task</p>
+        <button className="flex justify-center items-center mx-auto space-x-4 bg-green-400 w-[230px] h-[48px] rounded-full shadow-sm">
+          <AiOutlinePlus className="text-xl" />
+          <p className="text-lg font-bold">Create task</p>
         </button>
-        <p className='font-bold text-xl bg-green-200 h-[48px] flex items-center pl-6'>Calendar</p>
-        <div className='space-y-3 pl-6'>
-            <p className='font-bold text-xl'>Sort</p>
-            <ol>
-                <li className='font-semibold flex items-center'><IoEllipseSharp className='text-green-400 text-sm mr-2'/>Day</li>
-                <li className='font-semibold flex items-center'><IoEllipseSharp className='text-yellow-400 text-sm mr-2'/>Month</li>
-                <li className='font-semibold flex items-center'><IoEllipseSharp className='text-blue-400 text-sm mr-2'/>Year</li>
-            </ol>
-        </div>
-        <p className='pt-28'>
-            <BsFilterLeft className='text-4xl ml-6 text-green-400'/>
+        <p className="font-bold text-xl bg-green-200 h-[48px] flex items-center pl-6">
+          Calendar
         </p>
-       </div>
+        <div className="space-y-3 pl-6">
+          <p className="font-bold text-xl">Sort</p>
+          <ol>
+            <li className="font-semibold flex items-center">
+              <IoEllipseSharp className="text-green-400 text-sm mr-2" />
+              Day
+            </li>
+            <li className="font-semibold flex items-center">
+              <IoEllipseSharp className="text-yellow-400 text-sm mr-2" />
+              Month
+            </li>
+            <li className="font-semibold flex items-center">
+              <IoEllipseSharp className="text-blue-400 text-sm mr-2" />
+              Year
+            </li>
+          </ol>
+        </div>
+        <p className="pt-28">
+          <BsFilterLeft className="text-4xl ml-6 text-green-400" />
+        </p>
+      </div>
+
+      <div className="bg-nature bg-center bg-cover bg-no-repeat brightness-75 w-[80%] h-screen">
+        <div>
+            {/* <input type="text" placeholder="hello user"/> */}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Dashboard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'nature': 'url(https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg)',
+      }
+    },
   },
   plugins: [],
 }
