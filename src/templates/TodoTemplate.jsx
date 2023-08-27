3819bc41dd6cfcbba6ad9ccb2aa979338fbcc66e
Oshoaro Favour <oshoarofavour@gmail.com>
2023-08-27 01:23:57 +0100

    still working

--- src/templates/TodoTemplate.jsx
+++ src/templates/TodoTemplate.jsx
@@ -1,28 +1,17 @@
-// // import React, { useState } from 'react'
-
-// const TodoTemplate = (props) => {
-//   const toDoValue = props.propsToDo
-//   const value = props.props
-
-//   // console.log(toDoValue);
-//   // console.log(value);
-//   return (
-//     <section>
-//       {
-//         toDoValue
-//       }
-//     </section>
-//   )
-// }
-
-// export default TodoTemplate;
+import { IoEllipseSharp } from "react-icons/io5";
 
 const TodoTemplate = (props) => {
   const toDoList = props.propsToDo
 
   return (
-    <section>
-      {toDoList}
+    <section className="mt-10 bg-green-50 rounded w-[40%] ml-4">
+      <h2 className="text-center text-xl font-bold mb-3">Today's Task</h2>
+      {toDoList.map((toDo, index) => (
+        <div key={index} className="flex items-center space-x-2 mb-3 pl-2">
+          <IoEllipseSharp className="text-[8px]"/>
+          <p className="font-bold text-green-800">{toDo}</p>
+        </div>
+      ))}
     </section>
   )
 }
